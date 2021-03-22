30151b428473490348b823eb4ccc8ec599aeb79d
andrewy9 <andrew.yang993@gmail.com>
2021-03-22 22:46:22 +1300

    solved the Could not find a declaration file for module clarifai issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { Component } from 'react'
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
-import Particles from 'react-particles-js';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: `${process.env.CLIENT_SECRET}`
+});
+
+console.log(app)
 
 const particlesOptions = {
   particles: {
@@ -17,17 +25,45 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition/>*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+
+  }
+
+  onButonSubmit = () => {
+    console.log('click')
+    app.models.predict('a403429f2ddf4b49b307e318f00e528b', "https://samples.clarifai.com/face-det.jpg").then(
+      function (response) {
+        console.log(response)
+      },
+      err => {
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButonSubmit={this.onButonSubmit}
+        />
+        {/*<FaceRecognition/>*/}
+      </div>
+    );
+  }
 }
 
 export default App;
