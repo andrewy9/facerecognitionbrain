57c3db18a27d6d5b1fddedd434cf8a6bd780c5ce
andrewy9 <andrew.yang993@gmail.com>
2021-03-29 00:44:51 +1300

    fixed 3001 port error

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://sleepy-beach-76200.herokuapp.com/:3001/image', {
+          fetch('https://sleepy-beach-76200.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
